afc451c0582634fd5e939c1df653cac99e618040
Chase Ruskin <cruskin@umich.edu>
2025-01-30 22:29:14 -0700

    Refactors sv package

---
+++ .orbit/vsim.py
@@ -0,0 +1,165 @@
+from mod import Command, Env, Generic, Blueprint, Hdl
+import random
+import sys
+import os
+import argparse
+import glob
+import shutil
+
+class Verilator:
+
+    def __init__(self, args):
+        self.out_dir = 'build'
+        self.seed = args.seed
+        self.max_tests = args.loop_limit
+        self.skip_model = bool(args.skip_model)
+        self.lint_only = bool(args.lint)
+        self.use_vcd = bool(args.vcd)
+        self.events_log_file = str(args.log)
+        self.generics = args.generic
+        self.macros = args.define
+        self.line_coverage = bool(args.line_coverage)
+    
+    pass
+
+
+def main():
+    # Handle command-line arguments
+    parser = argparse.ArgumentParser(prog='vsim', allow_abbrev=False)
+
+    parser.add_argument('--lint', action='store_true', default=False, help='run static analysis and exit')
+    parser.add_argument('--generic', '-g', action='append', type=Generic.from_arg, default=[], metavar='KEY=VALUE', help='override top-level verilog parameters')
+    parser.add_argument('--vcd', action='store_true', default=False, help='enable saving vcd from testbench')
+    parser.add_argument('--line-coverage', action='store_true', default=False, help='enable line coverage')
+
+    parser.add_argument('--log', action='store', default='events.log', help='specify the log file path written during simulation')
+    parser.add_argument('--skip-model', action='store_true', help='skip execution of a design model (if exists)')
+    parser.add_argument('--seed', action='store', type=int, nargs='?', default=None, const=random.randrange(sys.maxsize), metavar='NUM', help='set the randomness seed')
+    parser.add_argument('--loop-limit', action='store', type=int, default=None, help='specify the limit of tests before timing out')
+
+    parser.add_argument('--define', '-d', action='append', type=str, default=[], metavar='KEY[=VALUE]', help='set preprocessor defines')
+
+    args = parser.parse_args()
+
+    V = Verilator(args)
+
+    py_model: str = None
+    rtl_order = []
+
+    # collect data from the blueprint
+    for rule in Blueprint().parse():
+        if rule.fileset == 'VLOG' or rule.fileset == 'SYSV':
+            rtl_order += [Hdl(rule.identifier, rule.path)]
+        elif rule.fileset == 'PYMDL':
+            py_model = rule.path
+        pass
+
+    HAS_MODEL = py_model != None
+
+    BENCH_NAME = Env.read('ORBIT_TB_NAME', missing_ok=False)
+
+    IP_PATH = Env.read("ORBIT_MANIFEST_DIR", missing_ok=False)
+
+    # format list of source files for verilator command-line
+    src_files = [str(src.path) for src in rtl_order]
+
+    # format generics for verilator command-line
+    top_gens = ['-G'+str(g.key)+'='+str(g.val) for g in V.generics]
+    top_macros = ['-D'+str(m) for m in V.macros]
+
+    # only perform lint and exit
+    if V.lint_only == True:
+        Command('verilator') \
+            .arg('--lint-only') \
+            .arg('--sv') \
+            .arg('--timing') \
+            .args(['-j', '4']) \
+            .args(top_gens) \
+            .args(['--Mdir', V.out_dir]) \
+            .arg('-Wall') \
+            .arg('-I'+str(IP_PATH)) \
+            .args(top_macros) \
+            .args(src_files) \
+            .spawn() \
+            .unwrap()
+        exit(0)
+
+    if HAS_MODEL == True and V.skip_model == False:
+        ORBIT_TB = Env.read("ORBIT_TB_NAME", missing_ok=False)
+        ORBIT_DUT = Env.read("ORBIT_DUT_NAME", missing_ok=False)
+
+        # export the interfaces using orbit to get the json data format
+        dut_data = Command("orbit").arg("get").arg(ORBIT_DUT).arg("--json").output()[0].strip()
+        tb_data = Command("orbit").arg("get").arg(ORBIT_TB).arg("--json").output()[0].strip()
+
+        seed_arg = '' if V.seed == None else '--seed='+str(V.seed)
+        loop_limit_arg = '' if V.max_tests == None else '--loop-limit='+str(V.max_tests)
+
+        Command("verb") \
+            .arg("model") \
+            .arg("--coverage").arg('coverage.txt') \
+            .arg(seed_arg) \
+            .arg(loop_limit_arg) \
+            .arg("--dut").arg(dut_data) \
+            .arg("--tb").arg(tb_data) \
+            .args(['-g=' + item.to_str() for item in V.generics]) \
+            .arg("python") \
+            .arg("--") \
+            .arg(py_model) \
+            .spawn() \
+            .unwrap()
+        pass
+
+    # overwrite top level parameters using -G<name>=<value>
+    # verilator --binary -j 0 -Wall our.v
+    Command('verilator') \
+        .arg('--binary') \
+        .arg('--sv') \
+        .arg('--trace' if V.use_vcd == True else '') \
+        .arg('--timing') \
+        .args(['--timescale', '1ns/10ps']) \
+        .args(['-j', '0']) \
+        .arg('--coverage-line' if V.line_coverage else '') \
+        .args(top_gens) \
+        .arg('-I'+str(IP_PATH)) \
+        .args(top_macros) \
+        .args(['--Mdir', V.out_dir]) \
+        .arg('-Wno-fatal') \
+        .args(['-o', BENCH_NAME]) \
+        .args(src_files) \
+        .spawn() \
+        .unwrap()
+
+    # run the executable
+    Command(V.out_dir + '/' + BENCH_NAME).spawn().unwrap()
+
+    # annotate the line coverage
+    if V.line_coverage == True:
+        Command('verilator_coverage') \
+            .arg('coverage.dat') \
+            .arg('-annotate') \
+            .arg('lines') \
+            .arg('--annotate-all') \
+            .spawn() \
+            .unwrap()
+        # rename all .sv files with a .cov extension to reduce confusion
+        files = glob.glob('lines/**/*.sv', recursive=True)
+        for f in files:
+            shutil.move(f, f+'.cov')
+
+    # check the event log
+    if HAS_MODEL == True and os.path.exists(V.events_log_file) == True:
+        Command('verb') \
+            .arg('check') \
+            .arg(V.events_log_file) \
+            .arg("--coverage=coverage.txt" if os.path.exists('coverage.txt') else None) \
+            .arg('--stats') \
+            .spawn() \
+            .unwrap()
+        pass
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
--- src/lib/hdl/src/godan.sv
+++ src/lib/hdl/src/godan.sv
@@ -1,8 +1,9 @@
 package godan;
 
+    // The default file to write to when events occur during simulation
     int FD_EVENTS = start("events.log");
 
-    /* tutils
+    /* UTILITY
 
         Provides test utility functions for testbench simulation. This involves
         reading interface signals, loading expected values, and various 
@@ -55,27 +56,24 @@ package godan;
         end
     endtask
 
+    // Synchronously set `pin` high, then asynchronously set `pin` low.
     task automatic sync_hi_async_lo(ref logic clk, ref logic pin, input int cycles);
         sync_on_async_off(clk, pin, 1'b1, cycles);
     endtask
 
-    // Synchronously triggers the logic bit `pin` to its state `active` and then 
-    // synchronously deactivates the bit to its initial value after `cycles`
-    // clock cycles elapse.
-    //
-    // The trigger will not be applied if `cycles` is set to 0. The signal will
-    // deactivate on the falling edge of the `cycles` count clock cycle.
-    task automatic sync_on_sync_off(ref logic clk, ref logic pin, input logic active, input int cycles);
-        automatic logic inactive = ~active;
-        if(cycles > 0) begin
-            @(posedge clk);
-            pin = active;
-            #0;
-            for(int i = 0; i < cycles; i++) begin
-                @(posedge clk);
-            end
-            pin = inactive;
-        end
+    // Synchronously set `pin` low, then asynchronously set `pin` high.
+    task automatic sync_lo_async_hi(ref logic clk, ref logic pin, input int cycles);
+        sync_on_async_off(clk, pin, 1'b0, cycles);
+    endtask
+
+    // Asynchronously set `pin` high, then synchronously set `pin` low.
+    task automatic async_hi_sync_lo(ref logic clk, ref logic pin, input int cycles);
+        async_on_sync_off(clk, pin, 1'b1, cycles);
+    endtask
+
+    // Asynchronously set `pin` low, then synchronously set `pin` high.
+    task automatic async_lo_sync_hi(ref logic clk, ref logic pin, input int cycles);
+        async_on_sync_off(clk, pin, 1'b0, cycles);
     endtask
 
     // Return a string in binary format by reading a logic value from the line `row`.
@@ -91,7 +89,7 @@ package godan;
         return sect;
     endfunction
 
-    /* events
+    /* EVENTS
     
         Data recording procedures to capture events of interest during 
         simulation.
@@ -99,123 +97,6 @@ package godan;
 
     // The log level type.
     typedef enum {TRACE, DEBUG, INFO, WARN, ERROR, FATAL} tone;
-
-    // Immediate assertion that checks if two logic words `received` and `expected` are equal to each other.
-    //
-    // Note: https://stackoverflow.com/questions/67714329/systemverilog-string-variable-as-format-specifier-for-display-write
-    task assert_eq(input logic[4095:0] received, input logic[4095:0] expected, input string subject);
-        // #0;
-        if(received == expected) begin
-            capture(FD_EVENTS, INFO, "ASSERT_EQ", subject, {"receives ", $sformatf("b'%0b", received), " and expects ", $sformatf("b'%0b", expected)});
-        end else begin
-            capture(FD_EVENTS, ERROR, "ASSERT_EQ", subject, {"receives ", $sformatf("b'%0b", received), " but expects ", $sformatf("b'%0b", expected)});
-        end
-    endtask
-
-    // Immediate assertion that checks if two logic words `received` and `expected` are equal to each other.
-    task assert_eq_old(inout int fd, input string received, input string expected, input string subject);
-        if(received == expected) begin
-            capture(fd, INFO, "ASSERT_EQ", subject, {"receives ", received, " and expects ", expected});
-        end else begin
-            capture(fd, ERROR, "ASSERT_EQ", subject, {"receives ", received, " but expects ", expected});
-        end
-    endtask
-
-    // Immediate assertion that checks if two logic words `received` and `expected` are not equal to each other.
-    task assert_ne(inout int fd, input string received, input string expected, input string subject);
-        if(received != expected) begin
-            capture(fd, INFO, "ASSERT_EQ", subject, {"receives ", received, " and does not expect ", expected});
-        end else begin
-            capture(fd, ERROR, "ASSERT_EQ", subject, {"receives ", received, " but does not expect ", expected});
-        end
-    endtask
-
-    // Checks the logic `flag` is true (1'b1) on the rising edge of `clk` before `cycles` clock cycles elapse.
-    task automatic observe(ref logic clk, ref logic flag, input logic active, input int cycles, input string subject);
-        automatic int cycle_count = 0;
-        automatic int cycle_limit = cycles;
-
-        if (cycle_limit < 0) begin
-            @(negedge clk, flag == active);
-            return;
-        end else begin
-            while (cycle_count < cycle_limit) begin
-                if (flag == active) begin
-                    capture(FD_EVENTS, INFO, "OBSERVE", subject, {"is true after waiting ", $sformatf("%-d", cycle_count), " cycle(s)"});
-                    break;
-                end
-                cycle_count = cycle_count + 1;
-                if (cycle_count < cycle_limit) begin
-                    @(negedge clk);
-                end
-            end
-        end
-
-        if(cycle_count >= cycle_limit) begin
-            capture(FD_EVENTS, ERROR, "OBSERVE", subject, {"fails to be true after waiting ", $sformatf("%-d", cycle_count), " cycle(s)"});
-        end
-    endtask;
-
-    // Concurrent assertion that checks the behavior of `data` is stable when the condition `flag` is true (1'b1).
-    task automatic assert_stbl(input bit flag, input logic[4095:0] data, input string subject);
-        static logic[4095:0] last_data[string];
-        static int last_flag[string];
-        static int cycles[string];
-        static int is_stable[string];
-
-        #0;
-        // stay in tracking state
-        if (last_flag.exists(subject) == 1 && last_flag[subject] == 1 && flag == 1'b1) begin
-            // things must remain stable!
-            if (last_data[subject] != data && is_stable[subject] == 1) begin
-                is_stable[subject] = 0;
-                capture(FD_EVENTS, ERROR, "ASSERT_STBL", subject, {"loses stability of ", $sformatf("b'%0b", last_data[subject]), " by changing to ", $sformatf("b'%0b", data), " after ", $sformatf("%-d", cycles[subject]), " cycle(s)"});
-            end
-            // survived another cycle
-            cycles[subject] = cycles[subject] + 1;
-        // successfully leave the tracking state
-        end else if (last_flag.exists(subject) == 1 && last_flag[subject] == 1 && flag == 1'b0) begin
-            if (is_stable.exists(subject) == 1 && is_stable[subject] == 1) begin
-                capture(FD_EVENTS, INFO, "ASSERT_STBL", subject, {"keeps stability at ", $sformatf("b'%0b", last_data[subject]), " for ", $sformatf("%-d", cycles[subject]), " cycle(s)"});
-            end
-        // try to transition into tracking state
-        end else if (last_flag.exists(subject) == 1 && last_flag[subject] == 0 && flag == 1'b1) begin
-            // $display("*** %s", subject);
-            cycles[subject] = 1;
-            is_stable[subject] = 1;
-        end
-
-        last_data[subject] = data;
-        last_flag[subject] = (flag == 1'b1) ? 1 : 0;
-    endtask
-
-    // Concurrent assertion that checks the behavior of `data` is stable when the condition `flag` is true (1'b1).
-    task automatic assert_stbl_old(inout int fd, ref logic clk, ref logic flag, ref string data, input string subject);
-        automatic logic is_okay = 1'b1;
-        automatic logic is_checked = 1'b0;
-        automatic string prev_data = "";
-        automatic int num_cycles = 0;
-        automatic string num_cycles_fmt = "";
-
-        @(posedge clk);
-
-        prev_data = data;
-        while(flag == 1'b1) begin
-            is_checked = 1'b1;
-            if(prev_data != data) begin
-                is_okay = 1'b0;
-                // capture
-                $sformat(num_cycles_fmt, "%-d", num_cycles);
-                capture(fd, ERROR, "ASSERT_STBL", subject, {"loses stability of ", prev_data, " by changing to ", data, " after ", num_cycles_fmt, " cycle(s)"});
-            end
-            @(posedge clk);
-            num_cycles = num_cycles + 1;
-        end
-        if(is_checked == 1'b1 && is_okay == 1'b1) begin
-            $sformat(num_cycles_fmt, "%-d", num_cycles);
-            capture(fd, INFO, "ASSERT_STBL", subject, {"keeps stability at ", prev_data, " for ", num_cycles_fmt, " cycle(s)"});
-        end
-    endtask
 
     // Captures an event during simulation and writes the outcome to the file `fd`.
     // The time when the task is called is recorded in the timestamp.
@@ -306,86 +187,83 @@ package godan;
         $fwrite(fd, {result, "\n"});
     endtask
 
-    /* macros
+    // Assertion that checks if two logic words `received` and `expected` are equal to each other.
+    //
+    // Note: https://stackoverflow.com/questions/67714329/systemverilog-string-variable-as-format-specifier-for-display-write
+    task assert_eq(input logic[4095:0] received, input logic[4095:0] expected, input string subject);
+        if(received == expected) begin
+            capture(FD_EVENTS, INFO, "ASSERT_EQ", subject, {"receives ", $sformatf("b'%0b", received), " and expects ", $sformatf("b'%0b", expected)});
+        end else begin
+            capture(FD_EVENTS, ERROR, "ASSERT_EQ", subject, {"receives ", $sformatf("b'%0b", received), " but expects ", $sformatf("b'%0b", expected)});
+        end
+    endtask
 
-        A nicer way to use Verb that resembles much of the API style as its
-        VHDL counterpart.
-    */
+    // Assertion that checks if two logic words `received` and `expected` are not equal to each other.
+    task assert_ne(input logic[4095:0] received, input logic[4095:0] expected, input string subject);
+        if(received != expected) begin
+            capture(FD_EVENTS, INFO, "ASSERT_NE", subject, {"receives ", $sformatf("b'%0b", received), " and does not expect ", $sformatf("b'%0b", expected)});
+        end else begin
+            capture(FD_EVENTS, ERROR, "ASSERT_NE", subject, {"receives ", $sformatf("b'%0b", received), " but does not expect ", $sformatf("b'%0b", expected)});
+        end
+    endtask
 
-// `ifndef VERB
-// `define VERB
+    // Assertion that checks that the behavior of `data` is stable while the condition `flag` is true (1'b1).
+    task automatic assert_stbl(input bit flag, input logic[4095:0] data, input string subject);
+        static logic[4095:0] last_data[string];
+        static int last_flag[string];
+        static int cycles[string];
+        static int is_stable[string];
 
-//     `define capture(LEVEL, TOPIC, SUBJECT, PREDICATE) \
-//         capture(FD_EVENTS, LEVEL, TOPIC, SUBJECT, PREDICATE);
+        #0;
+        // stay in tracking state
+        if (last_flag.exists(subject) == 1 && last_flag[subject] == 1 && flag == 1'b1) begin
+            // things must remain stable!
+            if (last_data[subject] != data && is_stable[subject] == 1) begin
+                is_stable[subject] = 0;
+                capture(FD_EVENTS, ERROR, "ASSERT_STBL", subject, {"loses stability of ", $sformatf("b'%0b", last_data[subject]), " by changing to ", $sformatf("b'%0b", data), " after ", $sformatf("%-d", cycles[subject]), " cycle(s)"});
+            end
+            // survived another cycle
+            cycles[subject] = cycles[subject] + 1;
+        // successfully leave the tracking state
+        end else if (last_flag.exists(subject) == 1 && last_flag[subject] == 1 && flag == 1'b0) begin
+            if (is_stable.exists(subject) == 1 && is_stable[subject] == 1) begin
+                capture(FD_EVENTS, INFO, "ASSERT_STBL", subject, {"keeps stability at ", $sformatf("b'%0b", last_data[subject]), " for ", $sformatf("%-d", cycles[subject]), " cycle(s)"});
+            end
+        // try to transition into tracking state
+        end else if (last_flag.exists(subject) == 1 && last_flag[subject] == 0 && flag == 1'b1) begin
+            // $display("*** %s", subject);
+            cycles[subject] = 1;
+            is_stable[subject] = 1;
+        end
 
-//     `define sync_hi_async_lo(CLK, DATA, CYCLES) \
-//         sync_on_async_off(CLK, DATA, 1'b1, CYCLES);
+        last_data[subject] = data;
+        last_flag[subject] = (flag == 1'b1) ? 1 : 0;
+    endtask
 
-//     `define async_hi_sync_lo(CLK, DATA, CYCLES) \
-//         async_on_sync_off(CLK, DATA, 1'b1, CYCLES);
+    // Checks the logic `flag` is true (1'b1) on the rising edge of `clk` before `cycles` clock cycles elapse.
+    task automatic observe(ref logic clk, ref logic flag, input logic active, input int cycles, input string subject);
+        automatic int cycle_count = 0;
+        automatic int cycle_limit = cycles;
 
-//     `define async_lo_sync_hi(CLK, DATA, CYCLES) \
-//         async_on_sync_off(CLK, DATA, 1'b0, CYCLES);
+        if (cycle_limit < 0) begin
+            @(negedge clk, flag == active);
+            return;
+        end else begin
+            while (cycle_count < cycle_limit) begin
+                if (flag == active) begin
+                    capture(FD_EVENTS, INFO, "OBSERVE", subject, {"is true after waiting ", $sformatf("%-d", cycle_count), " cycle(s)"});
+                    break;
+                end
+                cycle_count = cycle_count + 1;
+                if (cycle_count < cycle_limit) begin
+                    @(negedge clk);
+                end
+            end
+        end
 
-//     `define assert_eq(RECV, EXPT, SUBJECT) \
-//         begin \
-//             automatic string \RECV\ ; \
-//             automatic string \EXPT\ ; \
-//             $sformat(\RECV\ , "%b", RECV); \
-//             $sformat(\EXPT\ , "%b", EXPT); \
-//             assert_eq_old(FD_EVENTS, \RECV\ , \EXPT\ , SUBJECT); \
-//         end 
-
-//     `define assert_ne(RECV, EXPT, SUBJECT) \
-//         begin \
-//             automatic string \RECV\ ; \
-//             automatic string \EXPT\ ; \
-//             $sformat(\RECV\ , "%b", RECV); \
-//             $sformat(\EXPT\ , "%b", EXPT); \
-//             assert_ne(FD_EVENTS, \RECV\ , \EXPT\ , SUBJECT); \
-//         end 
-
-//     `define assert_stbl(CLK, FLAG, DATA, SUBJECT) \
-//         `ifndef \DATA\ \
-//         `define \DATA\ \
-//         string \DATA\ ; \
-//         `endif \
-//         always @(negedge clk) $sformat(\DATA\ , "%b", DATA); \
-//         always assert_stbl_old(FD_EVENTS, CLK, FLAG, \DATA\ , SUBJECT);
-
-//     // Analyzes the next part of the LINE into the logic value for X.
-//     `define parse(LINE, X) \
-//         $sscanf(parse(LINE), "%b", X);
-
-//     `define observe(CLK, FLAG, CYCLES, SUBJECT) \
-//         begin \
-//             automatic int cycle_count = 0; \
-//             automatic int cycle_limit = CYCLES + 1; \
-//             automatic string fmt_cycles; \
-//             if(cycle_limit < 0) begin \
-//                 @(negedge CLK, FLAG == 1'b1); \
-//             end else begin \
-//                 while(cycle_count < cycle_limit) begin \
-//                     if(FLAG == 1'b1) begin \
-//                         $sformat(fmt_cycles, "%-d", cycle_count); \
-//                         capture(FD_EVENTS, INFO, "OBSERVE", SUBJECT, {"is true after waiting ", fmt_cycles, " cycle(s)"}); \
-//                         break; \
-//                     end \
-//                     cycle_count = cycle_count + 1; \
-//                     if(cycle_count < cycle_limit) begin \
-//                         @(negedge CLK); \
-//                     end \
-//                 end \
-//             end \
-//             if(cycle_count >= cycle_limit) begin \
-//                 $sformat(fmt_cycles, "%-d", CYCLES); \
-//                 capture(FD_EVENTS, ERROR, "OBSERVE", SUBJECT, {"fails to be true after waiting ", fmt_cycles, " cycle(s)"}); \
-//             end \
-//         end
-
-//     `define complete() \
-//         complete(FD_EVENTS);
-
-// `endif
+        if(cycle_count >= cycle_limit) begin
+            capture(FD_EVENTS, ERROR, "OBSERVE", subject, {"fails to be true after waiting ", $sformatf("%-d", cycle_count), " cycle(s)"});
+        end
+    endtask;
 
 endpackage
